fc1d980df4e8d21504c6b3538ac7aac2fd289984
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-04 13:48:33 +0530

    Java

--- Day-1/Main.java
+++ Day-1/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.print("Hello World\n");
-        System.out.print("Hello World");
+        // System.out.println("Hello World\nJava");
+        // System.out.print("Hello World\n");
+        // System.out.print("Hello World");
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
     }
 }
