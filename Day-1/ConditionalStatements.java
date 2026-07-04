443f3d52b0ea48352136b80e84c0d6eaa8dcb93c
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-04 19:05:52 +0530

    Java

--- Day-1/ConditionalStatements.java
+++ Day-1/ConditionalStatements.java
@@ -5,6 +5,8 @@ public class ConditionalStatements {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // IF-ELSE
+
         // System.out.println("Enter your age: ");
         // int age = sc.nextInt();
 
@@ -15,13 +17,50 @@ public class ConditionalStatements {
         // }
 
 
-        System.out.println("Enter any number: ");
-        int n = sc.nextInt();
+        // System.out.println("Enter any number: ");
+        // int n = sc.nextInt();
 
-        if (n % 2 == 0) {
-            System.out.println("Even");
-        } else {
-            System.out.println("Odd");
+        // if (n % 2 == 0) {
+        //     System.out.println("Even");
+        // } else {
+        //     System.out.println("Odd");
+        // }
+
+
+        // ELSE-IF
+
+        // System.out.print("Enter any number: ");
+        // int a = sc.nextInt();
+
+        // System.out.print("Enter another number: ");
+        // int b = sc.nextInt();
+
+        // if (a == b) {
+        //     System.out.println("Equal");
+        // } else if (a > b) {
+        //     System.out.println("a is greater than b");
+        // } else {
+        //     System.out.println("b is greater than a");
+        // }
+
+
+        // SWITCH BREAK
+
+        // System.out.print("Press a button(1/2/3): ");
+        // int button = sc.nextInt();
+
+        // switch(button) {
+        //     case 1:
+        //         System.out.print("Hello");
+        //         break;
+        //     case 2:
+        //         System.out.print("Namaste");
+        //         break;
+        //     case 3:
+        //         System.out.print("Bonjour");
+        //         break;
+        //     default:
+        //         System.out.print("Invalid button");
         }
     }
 }
