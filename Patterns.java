a32906aae24ec178a3504e51853d744050cc77db
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-05 15:37:14 +0530

    Patterns

--- Patterns.java
+++ Patterns.java
@@ -68,11 +68,23 @@ public class Patterns {
         // int rows = sc.nextInt();
 
         // for (int i = 1; i <= rows; i++) {
-        //     for (int j = 1; j <= rows - i; j++) {
-        //         System.out.print("  ");
-        //     }
+        // for (int j = 1; j <= rows - i; j++) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+        // PATTERN 6 - half pyramid using numbers
+
+        // System.out.print("Enter number of rows: ");
+        // int rows = sc.nextInt();
+
+        // for (int i = 1; i <= rows; i++) {
         //     for (int j = 1; j <= i; j++) {
-        //         System.out.print("* ");
+        //         System.out.print(j + " ");
         //     }
         //     System.out.println();
         // }
