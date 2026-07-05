4ef30820fdfc03d367f7ac89be0d554368bb9d65
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-05 12:51:07 +0530

    Patterns

--- Patterns.java
+++ Patterns.java
@@ -4,19 +4,41 @@ public class Patterns {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // PATTERN 1
+        // PATTERN 1 - solid rectangle
 
-        System.out.print("Enter number of rows: ");
-        int rows = sc.nextInt();
+        // System.out.print("Enter number of rows: ");
+        // int rows = sc.nextInt();
 
-        System.out.print("Enter number of columns: ");
-        int cols = sc.nextInt();
+        // System.out.print("Enter number of columns: ");
+        // int cols = sc.nextInt();
 
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= cols; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
+        // for (int i = 1; i <= rows; i++) {
+        // for (int j = 1; j <= cols; j++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+        // PATTERN 2 - hollow rectangle
+
+        // System.out.print("Enter number of rows: ");
+        // int rows = sc.nextInt();
+
+        // System.out.print("Enter number of columns: ");
+        // int cols = sc.nextInt();
+
+        // for (int i = 1; i <= rows; i++) {
+        //     for (int j = 1; j <= cols; j++) {
+        //         if (i == 1 || i == rows || j == 1 || j == cols) {
+        //             System.out.print("* ");
+        //         } else {
+        //             System.out.print("  ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+
+        
     }
 }
