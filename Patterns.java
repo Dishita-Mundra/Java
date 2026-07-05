7b39e0890569ad8ef43f3a93cd10b52d13aa32cf
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-05 13:29:46 +0530

    Patterns

--- Patterns.java
+++ Patterns.java
@@ -39,6 +39,16 @@ public class Patterns {
         // }
 
 
-        
+        // PATTERN 3 - half pyramid
+
+        // System.out.print("Enter number of rows: ");
+        // int rows = sc.nextInt();
+
+        // for (int i = 1 ; i <= rows; i++){
+        //     for (int j = 1; j <= i; j++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
     }
 }
