bd9969cbfd566e1ee9400c3cf70b6ebe4fb208b5
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-05 14:12:45 +0530

    Patterns

--- Patterns.java
+++ Patterns.java
@@ -28,16 +28,15 @@ public class Patterns {
         // int cols = sc.nextInt();
 
         // for (int i = 1; i <= rows; i++) {
-        //     for (int j = 1; j <= cols; j++) {
-        //         if (i == 1 || i == rows || j == 1 || j == cols) {
-        //             System.out.print("* ");
-        //         } else {
-        //             System.out.print("  ");
-        //         }
-        //     }
-        //     System.out.println();
+        // for (int j = 1; j <= cols; j++) {
+        // if (i == 1 || i == rows || j == 1 || j == cols) {
+        // System.out.print("* ");
+        // } else {
+        // System.out.print(" ");
         // }
-
+        // }
+        // System.out.println();
+        // }
 
         // PATTERN 3 - half pyramid
 
@@ -45,7 +44,19 @@ public class Patterns {
         // int rows = sc.nextInt();
 
         // for (int i = 1 ; i <= rows; i++){
-        //     for (int j = 1; j <= i; j++){
+        // for (int j = 1; j <= i; j++){
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+        // PATTERN 4 - inverted half pyramid
+
+        // System.out.print("Enter number of rows: ");
+        // int rows = sc.nextInt();
+
+        // for (int i = rows; i >= 1; i--) {
+        //     for (int j = 1; j <= i; j++) {
         //         System.out.print("* ");
         //     }
         //     System.out.println();
