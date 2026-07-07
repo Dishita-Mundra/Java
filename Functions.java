4bdec10a8199021a4f5275411d44cf9d84df4b6f
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-07 21:36:30 +0530

    Functions

--- Functions.java
+++ Functions.java
@@ -18,41 +18,65 @@ public class Functions {
     // Q1. Write a function to print the sum of two numbers.
 
     // public static void sum(int a, int b) {
-    //     int sum = a + b;
-    //     System.out.print("Sum of " + a + " and " + b + " is " + sum);
-    //     return;
+    // int sum = a + b;
+    // System.out.print("Sum of " + a + " and " + b + " is " + sum);
+    // return;
     // }
 
     // public static void main(String[] args) {
-    //     Scanner sc = new Scanner(System.in);
+    // Scanner sc = new Scanner(System.in);
 
-    //     System.out.print("Enter first number: ");
-    //     int a = sc.nextInt();
+    // System.out.print("Enter first number: ");
+    // int a = sc.nextInt();
 
-    //     System.out.print("Enter second number: ");
-    //     int b = sc.nextInt();
+    // System.out.print("Enter second number: ");
+    // int b = sc.nextInt();
 
-    //     sum(a, b); // function call
+    // sum(a, b); // function call
     // }
-
 
     // Q2. Make a function to multiply two numbers and return the product.
 
     // public static void product(int a,int b){
-    //     int multiply = a * b;
-    //     System.out.print("Multiplication of " + a + " and " + b + " is " + multiply);
+    // int multiply = a * b;
+    // System.out.print("Multiplication of " + a + " and " + b + " is " + multiply);
+    // return;
+    // }
+
+    // public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+
+    // System.out.print("Enter first number: ");
+    // int a = sc.nextInt();
+
+    // System.out.print("Enter second number: ");
+    // int b = sc.nextInt();
+
+    // product(a, b); // function call
+    // }
+
+    // Q3. Find the factorial of a number.
+
+    // public static void factorial(int n) {
+    //     if (n < 0) {
+    //         System.out.print("Invalid number for factorial calculation");
+    //         return;
+    //     }
+
+    //     int fact = 1;
+    //     for (int i = n; i >= 1; i--) {
+    //         fact *= i;
+    //     }
+    //     System.out.println("factorial of " + n + " is " + fact);
     //     return;
     // }
 
     // public static void main(String[] args) {
     //     Scanner sc = new Scanner(System.in);
 
-    //     System.out.print("Enter first number: ");
-    //     int a = sc.nextInt();
+    //     System.out.print("Enter a number: ");
+    //     int n = sc.nextInt();
 
-    //     System.out.print("Enter second number: ");
-    //     int b = sc.nextInt();
-
-    //     product(a, b); // function call
+    //     factorial(n);
     // }
 }
