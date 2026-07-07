1a775f05f7f9a203ce39eb9231f9ae073fcad4bb
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-07 20:02:11 +0530

    Functions

--- Functions.java
+++ Functions.java
@@ -2,15 +2,37 @@ import java.util.*;
 
 public class Functions {
 
-    public static void printMyName(String name) {
-        System.out.println("Your name is " + name);
+    // public static void printMyName(String name) {
+    //     System.out.println("Your name is " + name);
+    //     return;
+    // }
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Enter your name: ");
+    //     String name = sc.nextLine();
+
+    //     printMyName(name);  // function call
+    // }
+
+
+    // Q1. Write a function to print the sum of two numbers.
+
+    public static void sum(int a,int b){
+        int sum=a+b;
+        System.out.print("Sum of " + a + " and " + b + " is " + sum);
         return;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = sc.nextLine();
 
-        printMyName(name);  // function call
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        sum(a,b);  // function call
     }
 }
