97dba4dde48a7c7408ca9c3f01ae003abb4f0599
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-08 11:32:09 +0530

    Practice

---
+++ Practice.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Practice {
+
+    // Q1. Enter 3 numbers from the user & make a function to print their average.
+
+    // public static void average(int a, int b, int c) {
+    //     float avg = (float) (a + b + c) / 3;
+    //     System.out.print("Anverage of given numbers is " + avg);
+    // }
+
+    // public static void main(String[] args) {
+    //      Scanner sc = new Scanner(System.in);
+
+    //      System.out.print("Enter first number: ");
+    //      int a = sc.nextInt();
+
+    //      System.out.print("Enter second number: ");
+    //      int b = sc.nextInt();
+
+    //      System.out.print("Enter third number: ");
+    //      int c = sc.nextInt();
+
+    //      average(a, b, c);
+
+    //      sc.close();
+    // }
+}
