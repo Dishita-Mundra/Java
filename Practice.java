18d3acc56f7ed973205d9f23877c7bbcefb21fdf
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-09 14:54:50 +0530

    Practice

--- Practice.java
+++ Practice.java
@@ -102,5 +102,27 @@ public class Practice {
     // }
 
 
-    
+    // Q5. Write a function that takes in age as input and returns if that person is eligible to vote or not. A person of age > 18 is eligible to vote.
+
+    // public static void votingAge(int age) {
+    //     if (age < 0) {
+    //         System.out.print("Invalid age");
+    //     } else if (age > 18) {
+    //         System.out.print("Eligible to vote");
+    //     } else {
+    //         System.out.print("Not eligible to vote");
+    //     }
+    //     return;
+    // }
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     System.out.print("Enter your age: ");
+    //     int age = sc.nextInt();
+
+    //     votingAge(age);
+
+    //     sc.close();
+    // }
 }
