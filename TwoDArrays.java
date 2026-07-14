a719e33f3485c7abd909fc58218f4d03ea28d5fc
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-14 19:44:19 +0530

    2-D Arrays

--- TwoDArrays.java
+++ TwoDArrays.java
@@ -82,11 +82,11 @@ public class TwoDArrays {
         // // input
         // // rows
         // for (int i = 0; i < n; i++) {
-        //     // columns
-        //     for (int j = 0; j < m; j++) {
-        //         System.out.print("Enter element of index " + "(" + i + "," + j + "): ");
-        //         matrix[i][j] = sc.nextInt();
-        //     }
+        // // columns
+        // for (int j = 0; j < m; j++) {
+        // System.out.print("Enter element of index " + "(" + i + "," + j + "): ");
+        // matrix[i][j] = sc.nextInt();
+        // }
         // }
         // System.out.println("The Spiral Order Matrix is : ");
         // int rowStart = 0;
@@ -96,30 +96,61 @@ public class TwoDArrays {
 
         // // To print spiral order matrix
         // while (rowStart <= rowEnd && colStart <= colEnd) {
-        //     // 1
-        //     for (int col = colStart; col <= colEnd; col++) {
-        //         System.out.print(matrix[rowStart][col] + " ");
-        //     }
-        //     rowStart++;
-        //     // 2
-        //     for (int row = rowStart; row <= rowEnd; row++) {
-        //         System.out.print(matrix[row][colEnd] + " ");
-        //     }
-        //     colEnd--;
-
-        //     // 3
-        //     for (int col = colEnd; col >= colStart; col--) {
-        //         System.out.print(matrix[rowEnd][col] + " ");
-        //     }
-        //     rowEnd--;
-
-        //     // 4
-        //     for (int row = rowEnd; row >= rowStart; row--) {
-        //         System.out.print(matrix[row][colStart] + " ");
-        //     }
-        //     colStart++;
-
-        //     System.out.println();
+        // // 1
+        // for (int col = colStart; col <= colEnd; col++) {
+        // System.out.print(matrix[rowStart][col] + " ");
         // }
+        // rowStart++;
+        // // 2
+        // for (int row = rowStart; row <= rowEnd; row++) {
+        // System.out.print(matrix[row][colEnd] + " ");
+        // }
+        // colEnd--;
+
+        // // 3
+        // for (int col = colEnd; col >= colStart; col--) {
+        // System.out.print(matrix[rowEnd][col] + " ");
+        // }
+        // rowEnd--;
+
+        // // 4
+        // for (int row = rowEnd; row >= rowStart; row--) {
+        // System.out.print(matrix[row][colStart] + " ");
+        // }
+        // colStart++;
+
+        // System.out.println();
+        // }
+
+        // Q. For a given matrix of N x M, print its transpose.
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int m = sc.nextInt();
+
+        int[][] numbers = new int[n][m];
+
+        // input
+        // rows
+        for (int i = 0; i < n; i++) {
+            // columns
+            for (int j = 0; j < m; j++) {
+                System.out.print("Enter element of index " + "(" + i + "," + j + "): ");
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("The transpose is : ");
+        // To print transpose
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < n; i++) {
+                System.out.print(numbers[i][j] + " ");
+            }
+        System.out.println();
+        }
     }
 }
