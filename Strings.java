0ec600db7fc05f0ca838431d8ba51b2037821654
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-18 00:27:20 +0530

    Strings

--- Strings.java
+++ Strings.java
@@ -20,7 +20,7 @@ public class Strings {
 
         // // charAt
         // for (int i = 0; i < fullName.length(); i++) {
-        //     System.out.println(fullName.charAt(i));
+        // System.out.println(fullName.charAt(i));
         // }
 
         // Compare Strings
@@ -28,36 +28,57 @@ public class Strings {
         // String name1 = "Tony";
         // String name2 = "Tony";
 
-        //1. s1 > s2: return +ve value
-        //2. s1 == s2: return 0
-        //3. s1 < s2: return -ve value
+        // 1. s1 > s2: return +ve value
+        // 2. s1 == s2: return 0
+        // 3. s1 < s2: return -ve value
 
         // if (name1.compareTo(name2) == 0 ) {
-        //     System.out.println("Strings are equal");
+        // System.out.println("Strings are equal");
         // } else {
-        //     System.out.println("Strings are not equal");
+        // System.out.println("Strings are not equal");
         // }
 
         // if (name1 == name2 ) {
-        //     System.out.println("Strings are equal");
+        // System.out.println("Strings are equal");
         // } else {
-        //     System.out.println("Strings are not equal");
+        // System.out.println("Strings are not equal");
         // }
 
         // WRONG METHOD
         // if (new String("Tony") == new String("Tony")) {
-        //     System.out.println("Strings are equal");
+        // System.out.println("Strings are equal");
         // } else {
-        //     System.out.println("Strings are not equal");
+        // System.out.println("Strings are not equal");
         // }
 
         // Getting Substring
 
-        String sentence = "My name is Dishita";
-        String name = sentence.substring(11, sentence.length());
-        System.out.println(name);
+        // String sentence = "My name is Dishita";
+        // String name = sentence.substring(11, sentence.length());
+        // System.out.println(name);
 
-        // Strings are Immutable - we directly can't change strings, we need to make new string to change the string 
+        // Strings are Immutable - we directly can't change strings, we need to make new
+        // string to change the string
 
-    }
+        // Q. Take an array of Strings input from the user & find the cumulative
+        // (combined) length of all those strings.
+
+    //     System.out.print("Enter size of array: ");
+    //     int size = sc.nextInt();
+
+    //     String array[] = new String[size];
+
+    //     int totalLen = 0;
+
+    //     for (int i = 0; i < size; i++) {
+
+    //         System.out.print("Enter string: ");
+    //         array[i] = sc.next();
+
+    //         totalLen += array[i].length();
+    //     }
+    //     System.out.println("Combined length of all strings: " + totalLen);
+    // }
+
+    
 }
