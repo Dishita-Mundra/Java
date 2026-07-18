ee2cb0d5de35af8918817fd327b5a017bb401add
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-18 12:51:49 +0530

    Strings

--- stringBuilder.java
+++ stringBuilder.java
@@ -3,13 +3,17 @@ import java.util.*;
 public class stringBuilder {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder("Tony");
-        System.out.println(sb);
+        System.out.println("Original String: " + sb);
 
-        // char at index 0
-        System.out.println(sb.charAt(0));
+        // // char at index 0
+        // System.out.println(sb.charAt(0));
 
-        // set char at index 0
-        sb.setCharAt(0, 'P');
-        System.out.println(sb);
+        // // set char at index 0
+        // sb.setCharAt(0, 'P');
+        // System.out.println(sb);
+
+        // insert
+        sb.insert(2, "n");
+        System.out.println("Updated String: " + sb);
     }
-} 
+}
