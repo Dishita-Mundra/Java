7684afa84dd8a13fcec67458391b8fa32c1e249c
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-18 23:15:16 +0530

    Strings

--- stringBuilder.java
+++ stringBuilder.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class stringBuilder {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("Tony");
+        StringBuilder sb = new StringBuilder("Dishita");
         System.out.println("Original String: " + sb);
 
         // // char at index 0
@@ -13,7 +13,11 @@ public class stringBuilder {
         // System.out.println(sb);
 
         // insert
-        sb.insert(2, "n");
-        System.out.println("Updated String: " + sb);
+        // sb.insert(2, "n");
+        // System.out.println("Updated String: " + sb);
+
+        // delete 
+        // sb.delete(4, 7);
+        // System.out.println("Updated String: " + sb);
     }
 }
